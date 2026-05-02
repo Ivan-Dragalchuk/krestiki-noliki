723f87900532129824634944dfab8dd77e83f68b
tetl <146548255+tetlovskei@users.noreply.github.com>
2026-05-02 14:52:31 +0300

    init

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+
+import json
+
+HOST, PORT = "0.0.0.0",320
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+sock.listen(100)
+
+def client_handler(client_socket,address):
+    pass
+
+
+while True:
+    client_socet, address = sock.accept()
+
+    threading.Thread(target=client_handler, daemon=True).start()
